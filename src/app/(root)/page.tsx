d0ef41d4173edad7eee4fb66ea0905feb78d29fa
pankaj-404 <67332663+soothsayervikas@users.noreply.github.com>
2024-05-20 18:58:34 +0530

    Implement Header box component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox"
 
-const Home = () => {
+const Home = async () => {
+  const loggedIn = {firstName: "Pankaj"}
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+          <header className="home-header">
+            <HeaderBox
+              type="greeting"
+              title="Welcome"
+              user={loggedIn.firstName || 'Guest'}
+              subtext="Access and manage your accounts and transactions"
+            />
+            
+          </header>
+      </div>
+
+      
+    </section>
   )
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,21 @@
+export const dynamic = 'force-dynamic'
+
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-const ibmPlexSerif = IBM_Plex_Serif({ subsets: ["latin"],
- variable: "--font-ibm-plex-serif",
- weight: ["400", "500", "600", "700"],
- });
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif'
+})
 
 export const metadata: Metadata = {
-  title: "Bank of Finance",
-  description: "Bank of Finance is a simple and efficient way to manage and spend your money. With Bank of Finance, you can manage your money in one place. It's free and easy to use. Start managing your money today!",
+  title: "Horizon",
+  description: "Horizon is a modern banking platform for everyone.",
   icons: {
-    icon: "/icons/logo.svg",
+    icon: '/icons/logo.svg'
   }
 };
 
