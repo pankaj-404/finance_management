5ce5322b4ede0cdc2b89ece8fba781ecd3d267d7
pankaj-404 <67332663+soothsayervikas@users.noreply.github.com>
2024-05-20 20:28:36 +0530

    Implement total balance component

---
+++ src/components/TotalBalanceBox.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import React from 'react'
+import CountUp from 'react-countup'
+import DoughnutChart from './DoughnutChart'
+import { chartSampleData } from '@/constants'
+
+const TotalBalanceBox = ({accounts=[],totalBanks,totalCurrentBalance}: TotalBalanceBoxProps) => {
+    const counterBalance = (totalCurrentBalance - 100) || 0
+    return (
+    <section className="total-balance">
+        <div className="total-balance-chart">
+            <DoughnutChart accounts={accounts} />
+        </div>
+        <div className="flex flex-col gap-6">
+            <h2 className="header-2">
+                Bank Accounts: {totalBanks}
+            </h2>
+            <div className="flex flex-col gap-2">
+                <p className="total-balance-label">
+                    Total Current Balance
+                </p>
+                <p className="total-balance-amount flex-center gap-2">
+                    <CountUp 
+                        duration={1}
+                        start={counterBalance > 0 ? counterBalance: 0 }
+                        end={totalCurrentBalance} 
+                        decimal='.' 
+                        decimals={2} 
+                        prefix='INR' 
+                    />
+                    
+                </p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default TotalBalanceBox
