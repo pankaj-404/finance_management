abe1b598bc39a584b92c40c29dd70bf5462cc747
pankaj-404 <67332663+soothsayervikas@users.noreply.github.com>
2024-05-20 20:28:09 +0530

    Add doughnut chart sample data

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -135,3 +135,30 @@ export const transactionCategoryStyles = {
     chipBackgroundColor: "bg-inherit",
   },
 };
+
+export const chartSampleData = {
+  labels: ["Bank1", "Bank2", "Bank3"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12000, 5999, 30000],
+      backgroundColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
