0cd7d162270149ec83912d3e8a8703920e15360a
pankaj-404 <67332663+soothsayervikas@users.noreply.github.com>
2024-05-23 19:01:08 +0530

    Remove unwanted code

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,10 +1,9 @@
 import AuthForm from '@/components/AuthForm'
-import { getLoggedInUser } from '@/lib/actions/user.actions';
+// import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
 const SignIn = async () => {
-    const loggedInUser = await getLoggedInUser();
-    console.log("🚀 ~ SignIn ~ loggedInUser:", loggedInUser)
+    // const loggedInUser = await getLoggedInUser();
 
   return (
     <section className='flex-center size-full max-sm:px-6'>
